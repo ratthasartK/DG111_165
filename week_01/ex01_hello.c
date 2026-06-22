edcecf02f2d60ab2cde7b3107e6f8fb146da03a1
RATTHASART KHADKEAW <armzaza404@gmail.com>
2026-06-22 16:08:51 +0700

    test

---
+++ week_01/ex01_hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("Name: Ratthasart khadkeaw\n");
+    printf("Student ID: 692110165\n");
+    printf("Class: DG111 computer programming\n");
+    return 0;
+}
